3752b053ca7b9887fdbf6cb11f2553d710b38449
yuryofujita <dasman74@hotmail.com>
2019-11-27 06:15:43 +0900

    lesson2 18 add

---
+++ lesson2/18-dataオプションにオブジェクトや配列を設定/lesson.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello vue js',
+        count: 10,
+        user: {
+            lastName: "yamada",
+            firstName: "taro",
+            prefecture: 'tokyo'
+        },
+        colors:['red','green', 'blue']
+    }
+})
